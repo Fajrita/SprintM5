8fe0640a80536b3e3f1ffd00b45a170353699d37
Fernanda Fajre <ffajrec@gmail.com>
2023-06-23 17:08:14 +0200

    contacto

--- src/main/java/cl/awakelab/ejercicio/controller/servlet/ServletContacto.java
+++ src/main/java/cl/awakelab/ejercicio/controller/servlet/ServletContacto.java
@@ -1,11 +1,18 @@
 package cl.awakelab.ejercicio.controller.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import cl.awakelab.ejercicio.controller.CapacitacionController;
 
 /**
  * Servlet implementation class Contacto
@@ -14,10 +21,26 @@ import javax.servlet.http.HttpServletResponse;
 public class ServletContacto extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		  
-		response.sendRedirect("contacto.jsp");
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	    // Crear una instancia del controlador
+	  
+	    // Leer los datos del cuerpo de la solicitud en formato JSON
+	    ObjectMapper mapper = new ObjectMapper();
+	    JsonNode jsonNode = mapper.readTree(request.getReader());
+	    String name = jsonNode.get("name").asText();
+	    String email = jsonNode.get("email").asText();
+	    String subject = jsonNode.get("subject").asText();
+	    String message = jsonNode.get("message").asText();
+	    System.out.println("Nombre: " + name);
+	    System.out.println("Correo: " + email);
+	    System.out.println("Asunto: " + subject);
+	    System.out.println("Mensaje: " + message);
+	   
+			
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.flush();
+		out.close();
 	}
 
 }
